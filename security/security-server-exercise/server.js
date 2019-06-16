2613cff6ba68f9572199d66ee79bacdccb7b3086
Archaeologist03 <omniiknight@hotmail.com>
2019-06-16 20:27:51 +0200

    Helment, COrs, Envs, Finished security section

--- security/security-server-exercise/server.js
+++ security/security-server-exercise/server.js
@@ -2,20 +2,35 @@ const express = require('express');
 const cors = require('cors');
 const morgan = require('morgan');
 var winston = require('winston');
+const helmet = reqire('helment');
+
 
 const bodyParser = require('body-parser');
 const app = express();
+
+app.use(helmet())
 app.use(bodyParser.json());
 app.use(cors());
 app.use(morgan('combined'));
 
-app.get('/', (req, res) => {
-  res.cookie('session', '1', { httpOnly: true });
-  res.cookie('session', '1', { secure: true });
+var whitelist = ['http://example1.com', 'http://example2.com', 'http://localhost:3000']
+var corsOptions = {
+  origin: function (origin, callback) {
+    if (whitelist.indexOf(origin) !== -1) {
+      callback(null, true)
+    } else {
+      callback(new Error('Not allowed by CORS'))
+    }
+  }
+}
 
-  res.set({
-    'Content-Security-Policy': "script-src 'self' 'https:/apis.google.com'",
-  });
+app.get('/', (req, res) => {
+  // res.cookie('session', '1', { httpOnly: true });
+  // res.cookie('session', '1', { secure: true });
+  // res.set({
+  //   'Content-Security-Policy': "script-src 'self' 'https:/apis.google.com'",
+  // });
+  
   res.send('Hello World!');
 });
 
